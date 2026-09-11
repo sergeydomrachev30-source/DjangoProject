3954b269ed43c01b4cd70ad4f61a9471d342be54
Sergey <new_day_has_come@mail.ru>
2026-09-11 11:51:38 +0300

    format settings with black

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,9 @@ load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-secret-key-for-github-actions')
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "django-insecure-fallback-secret-key-for-github-actions"
+)
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
